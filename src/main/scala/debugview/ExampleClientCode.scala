24c19b3c7e01cd5599276b9143be44f2acff28eb
yuhuan <jyuhuan@gmail.com>
2015-09-07 10:04:10 -0400

    is working now

--- src/main/scala/debugview/ExampleClientCode.scala
+++ src/main/scala/debugview/ExampleClientCode.scala
@@ -1,11 +1,9 @@
 package debugview
 
-import java.net.Socket
-
 /**
  * @author Yuhuan Jiang (jyuhuan@gmail.com).
  */
-object ExampleInClientCode extends App {
+object ExampleClientCode extends App {
 
   val htmlStr = "<html>Html page from the <strong>test client</strong>. </html>"
   RenderService.renderHtmlContent(htmlStr)
