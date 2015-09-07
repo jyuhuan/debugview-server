e2b4d7db86ac24914ddcdb91f36ffcd3399c2d0e
yuhuan <jyuhuan@gmail.com>
2015-09-07 10:27:56 -0400

    + tutorial

--- src/main/scala/debugview/client/package.scala
+++ src/main/scala/debugview/client/package.scala
@@ -2,27 +2,17 @@ package debugview
 
 import debugview.server._
 
-import java.net._
-
 /**
  * @author Tongfei Chen (ctongfei@gmail.com).
  */
 package object client {
 
-  val socket = new Socket("127.0.0.1", 1234)
-  val messenger = TcpMessenger(socket)
-  val segmentSize = 1024
-
   def renderHtml[T](x: T)(implicit f: HtmlRenderer[T]) = {
-    val content = f.str(x)
-    messenger.sendInt(TaskCode.RenderHtmlContent)
-    val segments = content.grouped(segmentSize).toSeq
-    messenger.sendStrings(segments)
+    RenderService.renderHtmlContent(f.str(x))
   }
 
   def renderHtmlAtPath(path: String) = {
-    messenger.sendInt(TaskCode.RenderHtmlContent)
-    messenger.sendString(path)
+    RenderService.renderHtmlAtPath(path)
   }
 
   def renderSvgContent(content: String) = ???
